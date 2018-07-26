b06c4d92bf5468418a32ffee17eff322da8a0050
mikeStr8s <michaelstrating@hotmail.com>
2018-07-26 13:17:02 -0400

    Further organization of logic

---
+++ board/tile.py
@@ -0,0 +1,6 @@
+class Tile:
+    def __init__(self):
+        self.tile_id = None
+        self.resource = None
+        self.value = None  # Numerical value for dice rolls
+        self.vertices = []
--- utils.py
+++ utils.py
@@ -2,3 +2,34 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+
+from math import pi, cos, sin
+
+
+def _get_color(resource):
+    if resource:
+        tile_colors = {
+            'brick': 'brown',
+            'wood': 'darkgreen',
+            'stone': 'grey',
+            'wheat': 'yellow',
+            'sheep': 'lightgreen',
+            'dessert': 'tan'
+        }
+        return tile_colors[resource]
+    return None
+
+
+def _hex_corner(self, i):
+    angle_deg = 60 * i - 30
+    angle_rad = pi / 180 * angle_deg
+    return tuple((self.center[0] + self.size * cos(angle_rad), self.center[1] + self.size * sin(angle_rad)))
+
+
+def _create_hex(self):
+    vertices = []
+    for x in range(0, 6):
+        vertices.append(self._hex_corner(x))
+    return vertices
+
