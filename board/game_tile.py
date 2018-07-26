b68e94eb8bfc20bc10afe8ab14358776a90dc1fd
mikeStr8s <michaelstrating@hotmail.com>
2018-07-26 12:56:23 -0400

    reorganization of logic

---
+++ board/game_tile.py
@@ -0,0 +1,46 @@
+from math import pi, cos, sin
+from collections import namedtuple
+
+
+class GameTile:
+    def __init__(self, tile_id, center, size):
+        self.tile_id = tile_id
+        self.resource = None
+        self.value = None
+        self.color = self._get_color(self.resource)
+        self.center = center
+        self.size = size
+        self.vertices = self._create_hex()
+        self.Point = namedtuple('Point', ['x', 'y'])
+
+    @staticmethod
+    def _get_color(resource):
+        if resource:
+            tile_colors = {
+                'brick': 'brown',
+                'wood': 'darkgreen',
+                'stone': 'grey',
+                'wheat': 'yellow',
+                'sheep': 'lightgreen',
+                'dessert': 'tan'
+            }
+            return tile_colors[resource]
+        return None
+
+    def _hex_corner(self, size, i):
+        angle_deg = 60 * i - 30
+        angle_rad = pi / 180 * angle_deg
+        return self.Point(self.center.x + size * cos(angle_rad), self.center.y + size * sin(angle_rad))
+
+    def _create_hex(self):
+        vertices = []
+        for x in range(0, 6):
+            vertices.append(self._hex_corner(self.size, x))
+        return vertices
+
+    def v_to_arr(self):
+        vert = []
+        for v in self.vertices:
+            vert.append(v.x)
+            vert.append(v.y)
+        return vert
---
+++ board/settlement.py
@@ -0,0 +1,4 @@
+class Settlement:
+    def __init__(self):
+        self.tiles = []
+        self.level = 1
